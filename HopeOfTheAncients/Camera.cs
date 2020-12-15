c9b9cbe73a3f8e19e540f08ddbf8ac22b59f27a5
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2020-12-15 23:01:09 +0100

    Remove createdContent dll
    * Remove engenious CreatedContent dll because its always autogenerated

---
+++ HopeOfTheAncients/Camera.cs
@@ -0,0 +1,27 @@
+﻿using engenious;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HopeOfTheAncients
+{
+    class Camera
+    {
+        public Matrix Projection { get; private set; }
+        public Matrix View { get; private set; }
+
+        public Matrix ViewProjection { get; private set; }
+
+        public void UpdateBounds(int width, int height)
+        {
+
+            Projection = Matrix.CreateOrthographicOffCenter(0, width, height, 0, -10, 10);
+        }
+        public void Update()
+        {
+            ViewProjection = Projection;
+        }
+    }
+}
