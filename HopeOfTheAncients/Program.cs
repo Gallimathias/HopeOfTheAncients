5c9336854639fda2a1c17c03afe38cc48db5a266
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2020-12-15 20:35:58 +0100

    Add engeniouse empty template
    * Add the enginiouse empty template as green empty working space

---
+++ HopeOfTheAncients/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HopeOfTheAncients
+{
+    public static class MainClass
+	{
+        [STAThread()]
+		public static void Main (string[] args)
+		{
+			using(var game = new HopeOfTheAncientsGame ())
+				game.Run();
+		}
+	}
+}
---
+++ HopeOfTheAncients/TestGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using engenious;
+
+namespace HopeOfTheAncients
+{
+    public class HopeOfTheAncientsGame : Game
+    {
+        protected override void Initialize()
+        {
+            var screenComponent = new ScreenComponent(this);
+
+            Components.Add(screenComponent);
+            base.Initialize();
+        }
+    }
+}
